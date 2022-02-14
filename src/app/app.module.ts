f865cc5e0777336c1c1a2d0f8e8e3ac917be959d
bartekbaran <50969096+bartekbaran@users.noreply.github.com>
2022-02-14 21:55:53 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,44 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainComponent } from './components/main/main.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { RouterModule,Routes } from "@angular/router";
+import { HotelListComponent } from './components/hotel-list/hotel-list.component';
+import { HotelComponent } from './components/hotel/hotel.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MatCardModule } from "@angular/material/card";
+import { HttpClientModule } from "@angular/common/http";
+import { SingleHotelComponent } from './components/single-hotel/single-hotel.component';
+
+const appRoutes:Routes = [
+  { path: '', component:HotelListComponent },
+  { path: 'hotel', component:SingleHotelComponent }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    HeaderComponent,
+    HotelListComponent,
+    HotelComponent,
+    FooterComponent,
+    SingleHotelComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: true }),
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
